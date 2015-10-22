c42805d2debd8d12549edbe787d8135f30a50924
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-10-22 14:31:45 +0200

    Add option to use railtie

--- lib/goomoji_translator.rb
+++ lib/goomoji_translator.rb
@@ -1,22 +1,12 @@
-class GomojiTranslator
-  GOOMOJI_CODES_FILE_NAME = 'codes.txt'
+require 'goomoji_translator/goomoji_translator'
+require "goomoji_translator/railtie" if defined?(Rails)
 
-  attr_reader :goomoji_codes
-  def initialize
-    file_path = File.absolute_path(File.dirname(__FILE__))
-    codes_list ||= begin
-      codes = File.read("#{file_path}/../files/#{GOOMOJI_CODES_FILE_NAME}")
-      codes.split("\n")
-    end
-
-    @goomoji_codes = {}
-    codes_list.each do |code|
-      goomoji_code, utf8_code = code.split(";")
-      @goomoji_codes[goomoji_code] = utf8_code
-    end
+module GoomojiTranslator
+  def self.dictionary
+    @dictionary ||= GoomojiTranslator.new
   end
 
-  def find_by_goomoji_code(goomoji_code)
-    goomoji_codes[goomoji_code]
+  def self.find_by_goomoji_code(goomoji_code)
+    dictionary.find_by_goomoji_code(goomoji_code)
   end
 end
---
+++ lib/goomoji_translator/railtie.rb
@@ -0,0 +1,7 @@
+require 'emoji'
+require 'rails'
+
+module GoomojiTranslator
+  class Railtie < Rails::Railtie
+  end
+end
